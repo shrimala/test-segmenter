9e7b359a5abf408aef7514ee5aaeb85fb820d50e
shivanandmn <shivanandnaduvin@gmail.com>
2023-11-02 14:57:04 +0530

    created new method

--- scorer.py
+++ scorer.py
@@ -1,5 +1,6 @@
 from DeepTiling.models.DeepTilingModels import DeepTiling
 import pandas as pd
+from sklearn.metrics.pairwise import cosine_similarity
 
 
 class DeepTilingSegmentScorer(DeepTiling):
@@ -14,14 +15,45 @@ class DeepTilingSegmentScorer(DeepTiling):
         self.text_data = text_data
         self.parameters = parameters
 
+    def compute_depth_score(
+        self,
+        sentences,
+        window,
+        **kwargs
+    ):
+        scores = []
+        for index in range(len(sentences) - 1):
+            if index <= window:
+                scores.append(
+                    cosine_similarity(
+                        sentences.iloc[: index + 1, :].mean().values.reshape(1, -1),
+                        sentences.iloc[index + 1 : index + window + 1, :]
+                        .mean()
+                        .values.reshape(1, -1),
+                    )[0][0]
+                )
+
+            else:
+                scores.append(
+                    cosine_similarity(
+                        sentences.iloc[index - window + 1 : index + 1, :]
+                        .mean()
+                        .values.reshape(1, -1),
+                        sentences.iloc[index + 1 : index + window + 1, :]
+                        .mean()
+                        .values.reshape(1, -1),
+                    )[0][0]
+                )
+        return scores, None
+
     def get_scores(self):
         embs = self.encoder.encode(self.text_data)
         embs = pd.DataFrame(embs)
-        scores, depth_scores = self.compute_depth_score(
+        scores, _ = self.compute_depth_score(
             embs,
             window=self.parameters.get("window", 10),
             clip=self.parameters.get("clip", 2),
             single=self.parameters.get("multi_encode", False),
             smooth=self.parameters.get("smooth", False),
         )
-        return [0.0]+scores
+        return [0.0] + scores
